3e85b56844141d3c9cdf3eb4714cdca072bc84b4
Matthias Neid <matthias@aternos.org>
2019-01-16 23:45:56 +0100

    initial commit

---
+++ src/Etcdserverpb/WatchClient.php
@@ -0,0 +1,34 @@
+<?php
+// GENERATED CODE -- DO NOT EDIT!
+
+namespace Etcdserverpb;
+
+/**
+ */
+class WatchClient extends \Grpc\BaseStub {
+
+    /**
+     * @param string $hostname hostname
+     * @param array $opts channel options
+     * @param \Grpc\Channel $channel (optional) re-use channel object
+     */
+    public function __construct($hostname, $opts, $channel = null) {
+        parent::__construct($hostname, $opts, $channel);
+    }
+
+    /**
+     * Watch watches for events happening or that have happened. Both input and output
+     * are streams; the input stream is for creating and canceling watchers and the output
+     * stream sends events. One watch RPC can watch on multiple key ranges, streaming events
+     * for several watches at once. The entire event history can be watched starting from the
+     * last compaction revision.
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function Watch($metadata = [], $options = []) {
+        return $this->_bidiRequest('/etcdserverpb.Watch/Watch',
+        ['\Etcdserverpb\WatchResponse','decode'],
+        $metadata, $options);
+    }
+
+}
