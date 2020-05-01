e2537addf564d125a87736cf2e8d4ae14744cffa
Jan Krpes <jan.krpes@cdn77.com>
2020-05-01 09:55:08 +0200

    Added lease methods

--- src/ClientInterface.php
+++ src/ClientInterface.php
@@ -3,6 +3,7 @@
 namespace Aternos\Etcd;
 
 use Aternos\Etcd\Exception\Status\InvalidResponseStatusCodeException;
+use Exception;
 
 /**
  * Interface ClientInterface
@@ -23,13 +24,13 @@ interface ClientInterface
      * @param string $key
      * @param mixed $value
      * @param bool $prevKv Get the previous key value in the response
-     * @param int $lease
+     * @param int $leaseID
      * @param bool $ignoreLease Ignore the current lease
      * @param bool $ignoreValue Updates the key using its current value
      * @return string|null Returns previous value if $prevKv is set to true
      * @throws InvalidResponseStatusCodeException
      */
-    public function put(string $key, $value, bool $prevKv = false, int $lease = 0, bool $ignoreLease = false, bool $ignoreValue = false);
+    public function put(string $key, $value, bool $prevKv = false, int $leaseID = 0, bool $ignoreLease = false, bool $ignoreValue = false);
 
     /**
      * Get a key value
@@ -71,4 +72,31 @@ interface ClientInterface
      * @throws InvalidResponseStatusCodeException
      */
     public function deleteIf(string $key, $previousValue, bool $returnNewValueOnFail = false);
+
+    /**
+     * Get leaseID which can be used with etcd's put
+     *
+     * @param int $ttl time-to-live in seconds
+     * @return int
+     * @throws InvalidResponseStatusCodeException
+     */
+    public function getLeaseID(int $ttl);
+
+    /**
+     * Revoke existing leaseID
+     *
+     * @param int $leaseID
+     * @throws InvalidResponseStatusCodeException
+     */
+    public function revokeLeaseID(int $leaseID);
+
+    /**
+     * Refresh chosen leaseID
+     *
+     * @param int $leaseID
+     * @return int lease TTL
+     * @throws InvalidResponseStatusCodeException
+     * @throws Exception
+     */
+    public function refreshLease(int $leaseID);
 }
